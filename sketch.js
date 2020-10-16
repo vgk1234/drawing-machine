2aadf33f11a4f76178d99e7ee0b27fd6edddbd2f
vgk1234 <vgk1234@github.io>
2020-10-15 23:37:00 -0700

    Updated Sketch

--- sketch.js
+++ sketch.js
@@ -23,11 +23,11 @@ function keyTyped () {
     saveCanvas('filename', 'png')
   }
 
-  beginShape(); 
-  for (let i = 0; i < array.length - 1; i++) {
-    curveVertex(array[i][0], array[i][1])
-  }
-  endShape();
+  // beginShape(); 
+  // for (let i = 0; i < array.length - 1; i++) {
+  //   curveVertex(array[i][0], array[i][1])
+  // }
+  // endShape();
   
   return false;
 }
