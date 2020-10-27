f2bf785edfcb43d5b4a05cbe5cc5611cf1ceb319
vgk1234 <vgk1234@github.io>
2020-10-26 19:53:25 -0700

    Updated Sketch

--- sketch.js
+++ sketch.js
@@ -4,34 +4,46 @@ let noiseOffset = 0.0;
 let strokeWidth = 5;
 
 function setup() {
-  //drawGrid();
-  createCanvas(600, 600); //(windowWidth, windowHeight)
+  createCanvas(800, 600); //(windowWidth, windowHeight)
+  drawGrid();
   //background(255, 255, 204); //(backgroundColor)
 
   stroke(255, 204, 0);
   strokeWeight(strokeWeight);
-
-  drawGrid();
 }
 
-function draw() {
-  if (mouseIsPressed) {
-    //background (220, 150, 0, 5); 
-    // backgroundColor-= 0.5;
-    strokeWeight(strokeWidth);
+function mousePressed () {
+  array = [];
+}
 
-    noiseOffset += 0.08;
-    strokeWidth = noise(noiseOffset) * 80;
+// function draw() {
+//   if (mouseIsPressed) {
+//     //background (220, 150, 0, 5); 
+//     // backgroundColor-= 0.5;
+//     strokeWeight(strokeWidth);
+
+//     noiseOffset += 0.08;
+//     strokeWidth = noise(noiseOffset) * 80;
 
 
-    stroke(map(mouseX, 0, 600, 0, 255, true))
-    line(width - mouseX, height - mouseY, 
-         width - pmouseX, height - pmouseY);
-    line(mouseX, mouseY, pmouseX, pmouseY);
-    line(mouseX, height - mouseY, 
-        pmouseX, height - pmouseY);
-    line(width - mouseX, mouseY, 
-         width - pmouseX, pmouseY);
+//     stroke(map(mouseX, 0, 600, 0, 255, true))
+//     line(width - mouseX, height - mouseY, 
+//          width - pmouseX, height - pmouseY);
+//     line(mouseX, mouseY, pmouseX, pmouseY);
+//     line(mouseX, height - mouseY, 
+//         pmouseX, height - pmouseY);
+//     line(width - mouseX, mouseY, 
+//          width - pmouseX, pmouseY);
+//   }
+// }
+
+
+function drawGrid() {
+  numCells = 20;
+  for (let i = 0; i <= width;  i += width / numCells); {
+    for (let j = 0; j <= height; j += height /numCells); {
+      rect (i, j, width / numCells, height / numCells);
+    }
   }
 }
 
@@ -50,13 +62,4 @@ function keyTyped () {
   // endShape();
   
   return false;
-}
-
-function drawGrid() {
-  numCells = 20;
-  for (let i = 0; i <= width;  i += width / numCells); {
-    for (let j = 0; j <= height; j += height /numCells); {
-      rect (i, j, width / numCells, height / numCells);
-    }
-  }
 }
